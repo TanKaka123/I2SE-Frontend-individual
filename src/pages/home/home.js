04e43b329a1690919180989a594450764adf805e
Hong-Tan <hongtan1422002@sgmail.com>
2022-11-12 01:34:16 +0700

    ui banner home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const home = () => {
     return (
         <div>
-            Home Page
+             <img alt="Thumbnail" src={require("../../assets/banner_home.png")} class="w-screen h-96" />
         </div>
     );
 };
